68424fff5d485f8754f000ac028b4aa919077df0
廾㠪〤Rɪᴅᴇʀ <84140090+HexRidder@users.noreply.github.com>
2021-05-21 09:54:50 +0530

    Update qr_cmd.py

--- bot/plugins/qr_cmd.py
+++ bot/plugins/qr_cmd.py
@@ -5,7 +5,7 @@ from messages import Msg
 async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.start}{Msg.source}",
+        text=Translation.start_msg",
         reply_to_message_id=message.message_id,
     )
 
@@ -13,6 +13,6 @@ async def start_message(client, message):
 async def help_message(client, message):
     await client.send_message(  
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.help}{Msg.source}",
+        text=Translation.help_msg",
         reply_to_message_id=message.message_id,
     )
